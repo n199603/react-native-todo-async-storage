465a13ed804e5950f19bf6256f09f773bfaf112d
Yuto Nakamura <yutonakamura0320@gmail.com>
2021-03-06 23:05:07 +0900

    feat(app.js): add react-native-paper

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import { Appbar, TextInput, Button } from "react-native-paper";
 
 export default function App() {
+  arr = [];
+  state = {
+    text: "",
+    item: [{ id: 1, data: "loading" }],
+  };
+
+  storedata = async () => {
+    this.arr.push(this.state.text);
+    await AsyncStorage.setItem("mylist", JSON.stringify(this.arr));
+    value = AsyncStorage.getItem("mylist");
+    console.log(value);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Appbar.Header>
+        <Appbar.Content title="ToDo" />
+      </Appbar.Header>
+      <TextInput
+        label="Add ToDo"
+        value={this.state.text}
+        onChangeText={(text) => this.setText({ text })}
+      />
+      <Text style={{ fontSize: 20 }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Button mode="contained" onPress={this.storedata}>
+        Add ToDo
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +42,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
