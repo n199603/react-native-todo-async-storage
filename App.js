86f4a144fb3ad7aa9a4faa5bb95af9513b5b49f8
Yuto Nakamura <yutonakamura0320@gmail.com>
2021-03-08 20:45:42 +0900

    feat(app.js): save data/ show todo

--- App.js
+++ App.js
@@ -3,40 +3,46 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AsyncStorage from "@react-native-community/async-storage";
 import { Appbar, TextInput, Button } from "react-native-paper";
+import { render } from "react-dom";
 
-export default function App() {
+export default class App extends React.Component {
   arr = [];
+  id = 0;
   state = {
     text: "",
     item: [{ id: 1, data: "loading" }],
   };
 
   storedata = async () => {
-    this.arr.push(this.state.text);
+    this.arr.push({ id: this.id, data: this.state.text });
+    this.id++;
     await AsyncStorage.setItem("mylist", JSON.stringify(this.arr));
-    value = AsyncStorage.getItem("mylist");
-    console.log(value);
+    this.setState({
+      item: JSON.parse(await AsyncStorage.getItem("mylist")),
+    });
+
+    console.log(this.state);
   };
 
-  return (
-    <View style={styles.container}>
-      <Appbar.Header>
-        <Appbar.Content title="ToDo" />
-      </Appbar.Header>
-      <TextInput
-        label="Add ToDo"
-        value={this.state.text}
-        onChangeText={(text) => this.setText({ text })}
-      />
-      <Text style={{ fontSize: 20 }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Button mode="contained" onPress={this.storedata}>
-        Add ToDo
-      </Button>
-      <StatusBar style="auto" />
-    </View>
-  );
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar.Header>
+          <Appbar.Content title="ToDo" />
+        </Appbar.Header>
+        <TextInput
+          label="Add ToDo"
+          value={this.state.text}
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text style={{ fontSize: 20 }}>{this.state.item[0].data}</Text>
+        <Button mode="contained" onPress={this.storedata}>
+          Add ToDo
+        </Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
